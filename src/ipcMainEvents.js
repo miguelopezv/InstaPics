9c6d4e566dd6aeaceb84516a9688f5c8fc14d09e
Miguel López <miguelopezv@gmail.com>
2018-04-08 13:29:57 -0500

    Move all ipcMain events to one file

---
+++ src/ipcMainEvents.js
@@ -0,0 +1,54 @@
+import { ipcMain, dialog } from 'electron'
+import isImage from 'is-image'
+import fs from 'fs'
+import path from 'path'
+import filesize from 'filesize'
+
+function setIpcMain (win) {
+  ipcMain.on('open-directory', (event) => {
+    dialog.showOpenDialog(win, {
+      title: 'select location',
+      buttonLabel: 'Open',
+      properties: ['openDirectory']
+    },
+    (dir) => {
+      const images = []
+      if (dir) {
+        fs.readdir(dir[0], (err, files) => {
+          if (err) throw err
+          files.forEach(f => {
+            if (isImage(f)) {
+              let imageFile = path.join(dir[0], f)
+              let stats = fs.statSync(imageFile)
+              let size = filesize(stats.size, {round: 0})
+              images.push({filename: f, src: `file://${imageFile}`, size: size})
+            }
+          })
+          event.sender.send('load-images', images)
+        })
+      }
+    })
+  })
+
+  ipcMain.on('open-save-dialog', (event, ext) => {
+    dialog.showSaveDialog(win, {
+      title: 'Save file',
+      buttonLabel: 'Save',
+      filters: [{name: 'image', extensions: [ext.substr(1)]}]
+    }, (file) => {
+      if (file) {
+        event.sender.send('save-image', file)
+      }
+    })
+  })
+
+  ipcMain.on('show-dialog', (event, info) => {
+    dialog.showMessageBox(win, {
+      type: info.type,
+      title: info.title,
+      message: info.message
+    })
+  })
+}
+
+module.exports = setIpcMain
